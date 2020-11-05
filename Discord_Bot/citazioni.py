874513007de7f1d4ebe4a12a713cb30cc4c3c759
SpaghettiHacks <67595441+SpaghettiHacks@users.noreply.github.com>
2020-11-05 18:59:26 +0100

    Add files via upload

---
+++ Discord_Bot/citazioni.py
@@ -0,0 +1,12 @@
+citazioni = {
+        1: "(Lao Tzu) | Fa più rumore un albero che cade di una foresta che cresce.",
+        2: "(Lev Tolstoj) | Tutti pensano a cambiare il mondo, ma nessuno pensa a cambiar se stesso.",
+        3: "(Mark Twain) | È meglio tenere la bocca chiusa e lasciare che le persone pensino che sei uno sciocco piuttosto che aprirla e togliere ogni dubbio.",
+        4: "(Confucio) | Colui che chiede è stupido per un minuto, colui che non chiede è stupido per tutta la vita.",
+        5: "(Charles Darwin) | Un uomo che osa sprecare anche solo un’ora del suo tempo non ha scoperto il valore della vita.",
+        6: "(Eleanor Roosevelt) | Fa ciò che senti giusto nel tuo cuore, poiché verrai criticato comunque. Sarai dannato se lo fai, dannato se non lo fai.",
+        7: "(William Shakespeare) | Se non ricordi che amore t’abbia mai fatto commettere la più piccola follia, allora non hai amato.",
+        8: "(Jim Morrison) | Alcuni dicono che la pioggia è brutta, ma non sanno che permette di girare a testa alta con il viso coperto dalle lacrime.",
+        9: "(Madre Teresa di Calcutta) | Se giudichi le persone, non avrai tempo per amarle.",
+        10: "(John Fitzgerald Kennedy) | Perdona i tuoi nemici, ma non dimenticare mai i loro nomi.",
+    }
